1da69149287fc94e93b734fa0dff89624c659018
Vitalii Khymynets <vtlkand@gmail.com>
2018-04-16 09:32:41 +0100

    S17-L302 Refactor reducers for readability

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -14,46 +14,57 @@ const INGRIDIENTS_PRICES = {
   meat: 1.20
 }
 
+const addIngridient = (state, action) => {
+  console.log('boo')
+  return {//use utility function for practice
+    ...state,
+    ingridients: {
+      ...state.ingridients,
+      [action.ingridientName]: state.ingridients[action.ingridientName] + 1
+    },
+    totalPrice: state.totalPrice + INGRIDIENTS_PRICES[action.ingridientName]
+  };
+};
+
+const removeIngridient = (state, action) => {
+  return {
+    ...state,
+    ingridients: {
+      ...state.ingridients,
+      [action.ingridientName]: state.ingridients[action.ingridientName] - 1
+    },
+    totalPrice: state.totalPrice - INGRIDIENTS_PRICES[action.ingridientName]
+  };
+}
+
+const setIngridients = (state, action) => {
+  return {
+    ...state,
+    ingridients: {
+      salad: action.ingridients.salad,
+      bacon: action.ingridients.bacon,
+      cheese: action.ingridients.cheese,
+      meat: action.ingridients.meat
+    },
+    error: false,
+    totalPrice: 2
+  };
+}
+
+const fetchIngridients = (state, action) => {
+  return {
+    ...state,
+    error: true
+  }
+}
+
 const reducer = (state = initialState, action) => {
   switch(action.type) {
-    case actionTypes.ADD_INGRIDIENT:
-      return {
-        ...state,
-        ingridients: {
-          ...state.ingridients,
-          [action.ingridientName]: state.ingridients[action.ingridientName] + 1
-        },
-        totalPrice: state.totalPrice + INGRIDIENTS_PRICES[action.ingridientName]
-
-      };
-    case actionTypes.REMOVE_INGRIDIENT:
-      return {
-        ...state,
-        ingridients: {
-          ...state.ingridients,
-          [action.ingridientName]: state.ingridients[action.ingridientName] - 1
-        },
-        totalPrice: state.totalPrice - INGRIDIENTS_PRICES[action.ingridientName]
-      };
-    case actionTypes.SET_INGRIDIENTS:
-      return {
-        ...state,
-        ingridients: {
-          salad: action.ingridients.salad,
-          bacon: action.ingridients.bacon,
-          cheese: action.ingridients.cheese,
-          meat: action.ingridients.meat
-        },
-        error: false,
-        totalPrice: 2
-      };
-    case actionTypes.FETCH_INGRIDIENTS_FAILD:
-      return {
-        ...state,
-        error: true
-      }
-    default:
-      return state;
+    case actionTypes.ADD_INGRIDIENT: return addIngridient(state, action);
+    case actionTypes.REMOVE_INGRIDIENT: return removeIngridient(state, action);
+    case actionTypes.SET_INGRIDIENTS: return setIngridients(state, action);
+    case actionTypes.FETCH_INGRIDIENTS_FAILD: return fetchIngridients(state, action);
+    default: return state;
   }
 };
 
